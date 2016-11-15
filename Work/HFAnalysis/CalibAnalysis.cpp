960fe03afa4503797ab26d0b6e7b3b438891ed13
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2016-11-14 23:59:54 -0600

    Implement QIE8 dual-anode channel with x2 and re-calibration. Also for iphi!=39

--- Work/HFAnalysis/CalibAnalysis.cpp
+++ Work/HFAnalysis/CalibAnalysis.cpp
@@ -30,6 +30,7 @@ using namespace std;
 int QIE10map(double eta, double phi, double depth);
 int QIE8map(double eta, double phi, double depth);
 bool ChanSel(int ieta, int iphi, int dep);
+double ADCtofC(double ADC);
 
 int main(int argc, char *argv[]){
 
@@ -49,7 +50,7 @@ int main(int argc, char *argv[]){
   //////////////////////////////////////
   int verbose = atoi(verbosity.c_str());
 
-  //KH 
+  //KH -- to be updated with more accurate pedestal values from database
   double pedestal_QIE10_depth1_anodeA[13]={
     12.6824, 13.1416, 13.3694, 12.0163, 12.4801, 15.3118, 14.5098, 14.6575, 13.8828, 12.4334, 15.6986, 0, 14.6407};
   double pedestal_QIE10_depth2_anodeA[13]={
@@ -59,6 +60,12 @@ int main(int argc, char *argv[]){
   double pedestal_QIE10_depth2_anodeB[13]={
     0., 15.8044, 0., 0., 0., 14.7922, 0., 0., 0., 0., 0., 0., 0.};
 
+  //KH -- Natalia's phi symmetry calibration constants for HF+ iphi=39 QIE8 channels - 15/07/2016
+  double calib_QIE8_iphi39_depth1[13]={
+    0.00,0.55,0.74,0.86,0.77,1.15,0.99,1.01,1.05,1.09,1.14,0.00,0.00};
+  double calib_QIE8_iphi39_depth2[13]={
+    0.00,0.00,0.75,0.78,0.86,0.00,0.91,0.86,0.80,0.85,0.85,0.00,0.00};
+
   //some varaibles
   char filenames[500];
   vector<string> filesVec;
@@ -66,7 +73,6 @@ int main(int argc, char *argv[]){
   TChain *sample_AUX = new TChain("hcalTupleTree/tree");
 
   char tempname[200];
-
 
   ///read the file names from the .txt files and load them to a vector.
   while(fin.getline(filenames, 500) ){filesVec.push_back(filenames);}
@@ -83,16 +89,42 @@ int main(int argc, char *argv[]){
   TH1D  HFEnergy      =  TH1D("HFEnergy","",105,-50.,1000.); // 10 GeV bin
   TH1D  HFCharge      =  TH1D("HFCharge","",200,0.,100.);    // 1 fC bin
 
+  // read pedestal for QIE10s
+  map<string,double> QIE10PedMap;
+  fstream pedFile("pedestals_26QIE10.txt", std::fstream::in);
+  string line;
+  int eta__,phi__,dep__;
+  string det__,DetId__;
+  double cap0,cap1,cap2,cap3,widthcap0, widthcap1, widthcap2, widthcap3;  
+  while (getline(pedFile, line)){
+    if (line.empty()) continue;
+    if (line[0] == '#') continue;
+    stringstream ss; ss << line;
+    ss >> eta__>>phi__>>dep__>>det__>>cap0>>cap1>>cap2>>cap3>>widthcap0>>widthcap1>>widthcap2>>widthcap3>>DetId__;
+    sprintf(tempname,"eta%d_phi%d_dep%d_capid%d",eta__,phi__,dep__,0);
+    QIE10PedMap[tempname]=ADCtofC(cap0);
+    sprintf(tempname,"eta%d_phi%d_dep%d_capid%d",eta__,phi__,dep__,1);
+    QIE10PedMap[tempname]=ADCtofC(cap1);
+    sprintf(tempname,"eta%d_phi%d_dep%d_capid%d",eta__,phi__,dep__,2);
+    QIE10PedMap[tempname]=ADCtofC(cap2);
+    sprintf(tempname,"eta%d_phi%d_dep%d_capid%d",eta__,phi__,dep__,3);
+    QIE10PedMap[tempname]=ADCtofC(cap3);    
+    printf("eta%d_phi%d_dep%d: cap0: %g cap1: %g cap2: %g cap3: %g\n",eta__,phi__,dep__,cap0,cap1,cap2,cap3);
+  } 
+  pedFile.close();
+
   // a vector of histograms for each ieta
   map<string,TH1D> Map_RecEnergy;
   map<string,TH1D> Map_Charge;
 
+  //
   // Loop over the events (tree entries)
+  //
   int eventN=0;
   while( evt->loadNext() ){
 
     // only analyze a limited number of events
-    //if(eventN>10000)break;
+    //if(eventN>1000)break;
 
     //if(eventN%1000==0)cout<< " ############# \nEvent#: " << eventN << endl; 
 
@@ -114,27 +146,42 @@ int main(int argc, char *argv[]){
 
       // initialize histograms
       if(eventN==0){
+      if (iphi==39 || iphi==35 || iphi==43){
         sprintf(tempname,"ieta%d_iphi%d_depth%d",ieta,iphi,depth);
         Map_RecEnergy[tempname]=HFEnergy;
         sprintf(tempname,"Charge_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
         Map_Charge[tempname]=HFCharge;
+
         if(iphi==39){
+	  sprintf(tempname,"ieta%d_iphinot%d_depth%d",ieta,iphi,depth);
+	  Map_RecEnergy[tempname]=HFEnergy;
+
           sprintf(tempname,"AnodeA_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
           Map_RecEnergy[tempname]=HFEnergy;
           sprintf(tempname,"AnodeB_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
           Map_RecEnergy[tempname]=HFEnergy;
+
+          sprintf(tempname,"AnodeB_QIE8x2_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
+          Map_RecEnergy[tempname]=HFEnergy;
+          sprintf(tempname,"AnodeB_QIE8calib_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
+          Map_RecEnergy[tempname]=HFEnergy;
+
           sprintf(tempname,"Charge_AnodeA_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
           Map_Charge[tempname]=HFCharge;
           sprintf(tempname,"Charge_AnodeB_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
           Map_Charge[tempname]=HFCharge;
         }
-      }
+
+      } // iphi==39 || iphi==35 || iphi==43
+      } // eventN==0
 
       double rcgain = evt->HFDigiRCGain_()->at(ich)[2];
+      /*
       std::cout << evt->HFDigiRCGain_()->at(ich)[0]
 		<< evt->HFDigiRCGain_()->at(ich)[1]
 		<< evt->HFDigiRCGain_()->at(ich)[2]
 		<< evt->HFDigiRCGain_()->at(ich)[3] << std::endl;
+      */
 
       //
       // both qie10 anods 
@@ -158,10 +205,23 @@ int main(int argc, char *argv[]){
             double qie1010ab=0., qie10a=0., qie10b=0.;
             for(int its_=0;its_<4;its_++){
 	      int ieta_bin=ieta-29;
-              qie1010ab += evt->QIE10DigiFC_()->at(qie10chA)[its_]-pedestal_QIE10_depth2_anodeA[ieta_bin] 
-		          +evt->QIE10DigiFC_()->at(qie10chB)[its_]-pedestal_QIE10_depth2_anodeB[ieta_bin]; // pedestal subtracted
-              qie10a += evt->QIE10DigiFC_()->at(qie10chA)[its_]-pedestal_QIE10_depth2_anodeA[ieta_bin];    // pedestal subtracted
-              qie10b += evt->QIE10DigiFC_()->at(qie10chB)[its_]-pedestal_QIE10_depth2_anodeB[ieta_bin];    // pedestal subtracted
+
+	      sprintf(tempname,"eta%d_phi%d_dep%d_capid%d",ieta,39,2,its_); 
+	      double qie10a_ped=QIE10PedMap[tempname];
+	      sprintf(tempname,"eta%d_phi%d_dep%d_capid%d",ieta,39,4,its_);
+	      double qie10b_ped=QIE10PedMap[tempname];
+
+	      /*
+	      std::cout << qie10a_ped << " " 
+			<< qie10b_ped << std::endl;
+	      std::cout << pedestal_QIE10_depth2_anodeA[ieta_bin] << " " 
+			<< pedestal_QIE10_depth2_anodeB[ieta_bin] << std::endl;
+	      */
+
+              qie1010ab += evt->QIE10DigiFC_()->at(qie10chA)[its_]-qie10a_ped 
+		          +evt->QIE10DigiFC_()->at(qie10chB)[its_]-qie10b_ped; // pedestal subtracted
+              qie10a += evt->QIE10DigiFC_()->at(qie10chA)[its_]-qie10a_ped;    // pedestal subtracted
+              qie10b += evt->QIE10DigiFC_()->at(qie10chB)[its_]-qie10b_ped;    // pedestal subtracted
             }
             //printf("1010:\nFCa: %g FCb: %g \n",evt->QIE10DigiFC_()->at(qie10chA)[its],evt->QIE10DigiFC_()->at(qie10chB)[its]);
             sprintf(tempname,"ieta%d_iphi%d_depth%d",ieta,iphi,depth);
@@ -191,10 +251,15 @@ int main(int argc, char *argv[]){
           if(evt->QIE10DigiSOI_()->at(qie10ch)[its]==1){
             double qie10a=0.,qie8b=0.;
 	    int ieta_bin=ieta-29;
-	    double ped=pedestal_QIE10_depth1_anodeA[ieta_bin];           // pedestal
+	    double ped;           // pedestal
 	    if (depth==2) ped=pedestal_QIE10_depth2_anodeA[ieta_bin];    // pedestal
+
             for(int its_=0;its_<4;its_++){
-              qie10a+=evt->QIE10DigiFC_()->at(qie10ch)[its_]-ped;
+
+	      sprintf(tempname,"eta%d_phi%d_dep%d_capid%d",ieta,iphi,depth+2,its_); 
+	      double qie10a_ped=QIE10PedMap[tempname];
+
+              qie10a+=evt->QIE10DigiFC_()->at(qie10ch)[its_]-qie10a_ped;
               qie8b +=evt->HFDigiFC_()->at(ich)[its_];
             }
             //printf("108:\nFCa: %g FCb: %g \n",evt->QIE10DigiFC_()->at(qie10ch)[its],evt->HFDigiFC_()->at(ich)[2]);
@@ -207,6 +272,15 @@ int main(int argc, char *argv[]){
             sprintf(tempname,"AnodeB_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
             //Map_RecEnergy[tempname].Fill(rcgain*(evt->HFDigiFC_()->at(ich)[2]+evt->HFDigiPedFC_()->at(ich)[2]));
             Map_RecEnergy[tempname].Fill(rcgain*(qie8b));
+
+	    //
+	    sprintf(tempname,"AnodeB_QIE8x2_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
+	    Map_RecEnergy[tempname].Fill(rcgain*(qie8b)*2);
+	    //
+	    double calib = calib_QIE8_iphi39_depth1[ieta_bin];
+	    if (depth==2) calib = calib_QIE8_iphi39_depth2[ieta_bin];
+	    sprintf(tempname,"AnodeB_QIE8calib_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
+	    Map_RecEnergy[tempname].Fill(rcgain*(qie8b)*2*calib);
 	    //
             sprintf(tempname,"Charge_ieta%d_iphi%d_depth%d",ieta,iphi,depth);
             Map_Charge[tempname].Fill(evt->QIE10DigiFC_()->at(qie10ch)[0]+evt->HFDigiFC_()->at(ich)[0]);
@@ -241,14 +315,16 @@ int main(int argc, char *argv[]){
 	       );
 	*/
         //Map_RecEnergy[tempname].Fill(rcgain*(evt->HFDigiFC_()->at(ich)[2]+evt->HFDigiPedFC_()->at(ich)[2])); // pedestal is added back
-        Map_RecEnergy[tempname].Fill(rcgain*(qie8b));
+	if (iphi==39 || iphi==35 || iphi==43)	
+	  Map_RecEnergy[tempname].Fill(rcgain*(qie8b));
+	
+        sprintf(tempname,"ieta%d_iphinot39_depth%d",ieta,depth);
+	Map_RecEnergy[tempname].Fill(rcgain*(qie8b));
+
         //if(evt->HFDigiFC_()->at(ich)[2]+evt->HFDigiPedFC_()->at(ich)[2] < 0.)cout << "HFFC: " << evt->HFDigiFC_()->at(ich)[2] << " HFPedFC: " << evt->HFDigiPedFC_()->at(ich)[2] << endl;
-      }
+      } // iphi!=39
 
-
-
-
-    }
+    }   // loop over channels
 
     eventN++;
   }// End of loop over events
@@ -317,8 +393,84 @@ int QIE10map(double eta, double phi, double depth){
 bool ChanSel(int ieta, int iphi, int dep){
 
   //if(dep!=2) return true;
-  if(iphi!=35 && iphi!=39 && iphi!=43 )return true;
+  //if(iphi!=35 && iphi!=39 && iphi!=43 )return true;
   if(ieta!=29&&ieta!=30&&ieta!=31&&ieta!=32&&ieta!=33&&ieta!=34&&ieta!=35&&ieta!=36&&ieta!=37&&ieta!=38&&ieta!=39&& ieta!=41)return true;
 
   return false; 
+}
+
+double ADCtofC(double ADC){
+
+  // NEEDS UPDATING
+  double adc2fC_QIE10[256]={
+  // - - - - - - - range 0 - - - - - - - -
+  //subrange0
+  1.58, 4.73, 7.88, 11.0, 14.2, 17.3, 20.5, 23.6,
+  26.8, 29.9, 33.1, 36.2, 39.4, 42.5, 45.7, 48.8,
+  //subrange1
+  53.6, 60.1, 66.6, 73.0, 79.5, 86.0, 92.5, 98.9,
+  105, 112, 118, 125, 131, 138, 144, 151,
+  //subrange2
+  157, 164, 170, 177, 186, 199, 212, 225,
+  238, 251, 264, 277, 289, 302, 315, 328,
+  //subrange3
+  341, 354, 367, 380, 393, 406, 418, 431,
+  444, 464, 490, 516, 542, 568, 594, 620,
+
+  // - - - - - - - range 1 - - - - - - - -
+  //subrange0
+  569, 594, 619, 645, 670, 695, 720, 745,
+  771, 796, 821, 846, 871, 897, 922, 947,
+  //subrange1
+  960, 1010, 1060, 1120, 1170, 1220, 1270, 1320,
+  1370, 1430, 1480, 1530, 1580, 1630, 1690, 1740,
+  //subrange2
+  1790, 1840, 1890, 1940,  2020, 2120, 2230, 2330,
+  2430, 2540, 2640, 2740, 2850, 2950, 3050, 3150,
+  //subrange3
+  3260, 3360, 3460, 3570, 3670, 3770, 3880, 3980,
+  4080, 4240, 4450, 4650, 4860, 5070, 5280, 5490,
+
+  // - - - - - - - range 2 - - - - - - - -
+  //subrange0
+  5080, 5280, 5480, 5680, 5880, 6080, 6280, 6480,
+  6680, 6890, 7090, 7290, 7490, 7690, 7890, 8090,
+  //subrange1
+  8400, 8810, 9220, 9630, 10000, 10400, 10900, 11300,
+  11700, 12100, 12500, 12900, 13300, 13700, 14100, 14500,
+  //subrange2
+  15000, 15400, 15800, 16200, 16800, 17600, 18400, 19300,
+  20100, 20900, 21700, 22500, 23400, 24200, 25000, 25800,
+  //subrange3
+  26600, 27500, 28300, 29100, 29900, 30700, 31600, 32400,
+  33200, 34400, 36100, 37700, 39400, 41000, 42700, 44300,
+
+  // - - - - - - - range 3 - - - - - - - - -
+  //subrange0
+  41100, 42700, 44300, 45900, 47600, 49200, 50800, 52500,
+  54100, 55700, 57400, 59000, 60600, 62200, 63900, 65500,
+  //subrange1
+  68000, 71300, 74700, 78000, 81400, 84700, 88000, 91400,
+  94700, 98100, 101000, 105000, 108000, 111000, 115000, 118000,
+  //subrange2
+  121000, 125000, 128000, 131000, 137000, 145000, 152000, 160000,
+  168000, 176000, 183000, 191000, 199000, 206000, 214000, 222000,
+  //subrange3
+  230000, 237000, 245000, 253000, 261000, 268000, 276000, 284000,
+  291000, 302000, 316000, 329000, 343000, 356000, 370000, 384000
+  };
+
+  int ADC_lo = floor(ADC);
+  int ADC_hi = ceil(ADC);
+  
+  double Charge_lo = adc2fC_QIE10[ADC_lo];
+  double Charge_hi = adc2fC_QIE10[ADC_hi];
+  
+  double Charge = Charge_lo;
+  if (ADC_hi>ADC_lo) 
+    Charge += (Charge_hi-Charge_lo)
+      *(ADC-double(ADC_lo))/(double(ADC_hi-ADC_lo));
+
+  return Charge;
+  
 }
--- Work/HFAnalysis/plot_spectra.C
+++ Work/HFAnalysis/plot_spectra.C
@@ -1,4 +1,29 @@
 
+//
+// --- emean  ---
+//
+double emean(TH1F* htmp, double high, double low){
+
+  //htmp->Print();
+  double tot_val=0.;
+  double tot_ent=0.;
+  for (int ibin=0;ibin<htmp->GetNbinsX();ibin++){
+    double bin_value = htmp->GetBinCenter(ibin+1);
+    double bin_ent   = htmp->GetBinContent(ibin+1);
+    if (bin_value>low && bin_value<high){
+      tot_ent += bin_ent;
+      tot_val += bin_ent*bin_value;
+    }
+  }
+  double emean = 0.;
+  if (tot_ent>0.) emean = tot_val/tot_ent;
+  return emean;
+
+}
+
+//
+// plot spectra
+//
 void plot_spectra(){
 
   //
@@ -16,9 +41,19 @@ void plot_spectra(){
 
   TFile *_file0 = TFile::Open("OutPut/results_KH3.root");
   TH1F *energy_anode_iphi39[13][2][2]; // ieta=29-41:13, depth:2, anode A&B:2
+  TH1F *energy_QIE8x2_iphi39[13][2];     // ieta=29-41:13, depth:2, QIE8 x2 (as done for default reco)
+  TH1F *energy_QIE8calib_iphi39[13][2];  // ieta=29-41:13, depth:2, QIE8 phi symmetry corrections
   TH1F *energy_iphi39[13][2];          // ieta=29-41:13, depth:2
   TH1F *energy_iphi35[13][2];          // ieta=29-41:13, depth:2
   TH1F *energy_iphi43[13][2];          // ieta=29-41:13, depth:2
+  TH1F *energy_iphinot39[13][2];       // ieta=29-41:13, depth:2
+
+  double emean_hi=400.;
+  double emean_lo=40.;
+  TH1F *relative_response_vsieta_iphi39_depth1 = new TH1F("relative_response_vsieta_iphi39_depth1","relative_response_vsieta_iphi39_depth1",13,28.5,41.5);
+  TH1F *relative_response_vsieta_iphi39_depth2 = new TH1F("relative_response_vsieta_iphi39_depth2","relative_response_vsieta_iphi39_depth2",13,28.5,41.5);
+  TH1F *relative_response_iphi39_depth1 = new TH1F("relative_response_iphi39_depth1","relative_response_iphi39_depth1",100,0.5,1.5);
+  TH1F *relative_response_iphi39_depth2 = new TH1F("relative_response_iphi39_depth2","relative_response_iphi39_depth2",100,0.5,1.5);
 
   char tmp[40];
 
@@ -31,13 +66,24 @@ void plot_spectra(){
 
       sprintf(tmp,"ieta%d_iphi39_depth%d",ieta,idepth);
       //std::cout << tmp << std::endl;
-      energy_iphi35[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);
+      energy_iphi39[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);       // iphi=39 
+
+      sprintf(tmp,"ieta%d_iphinot39_depth%d",ieta,idepth);
+      energy_iphinot39[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);    // iphi!=39
+      if (ieta>=40) energy_iphinot39[ieta_bin][idepth_bin]->Scale(1./17.); // nphi=18-1
+      else          energy_iphinot39[ieta_bin][idepth_bin]->Scale(1./35.); // nphi=36-1
 
       sprintf(tmp,"ieta%d_iphi35_depth%d",ieta,idepth);
-      energy_iphi39[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);
+      energy_iphi35[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);       // iphi=35
 
       sprintf(tmp,"ieta%d_iphi43_depth%d",ieta,idepth);
-      energy_iphi43[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);
+      energy_iphi43[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);       // iphi=43
+
+      sprintf(tmp,"AnodeB_QIE8x2_ieta%d_iphi39_depth%d",ieta,idepth);
+      energy_QIE8x2_iphi39[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);          // iphi=39 
+
+      sprintf(tmp,"AnodeB_QIE8calib_ieta%d_iphi39_depth%d",ieta,idepth);
+      energy_QIE8calib_iphi39[ieta_bin][idepth_bin] = (TH1F*)_file0->Get(tmp);       // iphi=39 
 
       for (int ianode_bin=0; ianode_bin<=1; ianode_bin++){
 	if      (ianode_bin==0) sprintf(tmp,"AnodeA_ieta%d_iphi39_depth%d",ieta,idepth);
@@ -51,7 +97,7 @@ void plot_spectra(){
 
 
   //
-  // Plotting
+  // Plotting - Depth1
   //
   TCanvas *c1 = new TCanvas("c_HFP_depth1","c1_HFP_depth1",1600,1200);
   TLegend* catLeg1 = new TLegend(0.55,0.5,0.9,0.85);
@@ -85,26 +131,144 @@ void plot_spectra(){
     energy_iphi39[ieta_bin][0]->SetLineColor(1);
     energy_iphi35[ieta_bin][0]->SetLineColor(2);
     energy_iphi43[ieta_bin][0]->SetLineColor(4);
+    energy_iphinot39[ieta_bin][0]->SetLineColor(6);
 
     sprintf(tmp,"ieta = %d, depth=1",ieta);
     energy_iphi39[ieta_bin][0]->SetTitle(tmp);
     energy_iphi39[ieta_bin][0]->Draw();
     energy_iphi35[ieta_bin][0]->Draw("same");
     energy_iphi43[ieta_bin][0]->Draw("same");
+    //energy_iphinot39[ieta_bin][0]->Draw("same");
 
     if (ieta_bin==0){
       catLeg1->AddEntry(energy_iphi35[ieta_bin][0],"iphi=35: Q #times gain","l");
       catLeg1->AddEntry(energy_iphi39[ieta_bin][0],"iphi=39: Q_{A}+Q_{B} #times gain","l");
       catLeg1->AddEntry(energy_iphi43[ieta_bin][0],"iphi=43: Q #times gain","l");
+      //catLeg1->AddEntry(energy_iphinot39[ieta_bin][0],"iphi#neq39: Q #times gain","l");
     }
     catLeg1->Draw();
+
+    std::cout << ieta << " "
+              << emean(energy_iphi35[ieta_bin][0],emean_hi,emean_lo) << " "
+              << emean(energy_iphi39[ieta_bin][0],emean_hi,emean_lo) << " "
+              << emean(energy_iphi43[ieta_bin][0],emean_hi,emean_lo) << std::endl;
+    relative_response_vsieta_iphi39_depth1->SetBinContent(ieta_bin+1,
+                                                   2.*emean(energy_iphi39[ieta_bin][0],emean_hi,emean_lo)
+                                                   /(emean(energy_iphi35[ieta_bin][0],emean_hi,emean_lo)+emean(energy_iphi43[ieta_bin][0],emean_hi,emean_lo)));    
+    relative_response_vsieta_iphi39_depth1->SetBinError(ieta_bin+1,0.);
+    relative_response_iphi39_depth1->Fill(
+                                                   2.*emean(energy_iphi39[ieta_bin][0],emean_hi,emean_lo)
+                                                   /(emean(energy_iphi35[ieta_bin][0],emean_hi,emean_lo)+emean(energy_iphi43[ieta_bin][0],emean_hi,emean_lo)));
    
   }
   c1->SaveAs("c_HFP_depth1.gif");
   c1->SaveAs("c_HFP_depth1.pdf");
 
   //
+  // Plotting
   //
+  TCanvas *c1b = new TCanvas("c_HFP_depth1_b","c1b_HFP_depth1",1600,1200);
+  TLegend* catLeg1b = new TLegend(0.55,0.5,0.9,0.85);
+  catLeg1b->SetTextSize(0.064);
+  catLeg1b->SetTextFont(42);
+  catLeg1b->SetLineColor(0);
+
+  c1b->Divide(3,4);
+  for (int ieta_bin=0; ieta_bin<=12; ieta_bin++){
+    int ieta=ieta_bin+29;
+    if (ieta==40) continue;
+    if (ieta>40) c1b->cd(ieta_bin); // skip ieta=40
+    else         c1b->cd(ieta_bin+1);
+    gPad->SetLogy();
+    gPad->SetTopMargin(0.02);
+    gPad->SetBottomMargin(0.16);
+    gPad->SetRightMargin(0.04);
+    gPad->SetLeftMargin(0.16);
+
+    energy_iphinot39[ieta_bin][0]->SetLineColor(6);
+
+    sprintf(tmp,"ieta = %d, depth=1",ieta);
+    energy_iphi39[ieta_bin][0]->SetTitle(tmp);
+    energy_iphi39[ieta_bin][0]->Draw();
+    energy_iphinot39[ieta_bin][0]->Draw("same");
+
+    if (ieta_bin==0){
+      catLeg1b->AddEntry(energy_iphi39[ieta_bin][0],"iphi=39: Q_{A}+Q_{B} #times gain","l");
+      catLeg1b->AddEntry(energy_iphinot39[ieta_bin][0],"iphi#neq39: Q #times gain","l");
+    }
+    catLeg1b->Draw();
+   
+  }
+  c1b->SaveAs("c_HFP_depth1_b.gif");
+  c1b->SaveAs("c_HFP_depth1_b.pdf");
+
+  //
+  // Plotting
+  //
+  TCanvas *c1c = new TCanvas("c_HFP_depth1_c","c1c_HFP_depth1",1600,1200);
+  TLegend* catLeg1c = new TLegend(0.55,0.5,0.9,0.85);
+  catLeg1c->SetTextSize(0.064);
+  catLeg1c->SetTextFont(42);
+  catLeg1c->SetLineColor(0);
+  TLegend* catLeg1c2 = new TLegend(0.55,0.5,0.9,0.85);
+  catLeg1c2->SetTextSize(0.064);
+  catLeg1c2->SetTextFont(42);
+  catLeg1c2->SetLineColor(0);
+
+  c1c->Divide(3,4);
+  for (int ieta_bin=0; ieta_bin<=12; ieta_bin++){
+    int ieta=ieta_bin+29;
+    if (ieta==40) continue;
+    if (ieta>40) c1c->cd(ieta_bin); // skip ieta=40
+    else         c1c->cd(ieta_bin+1);
+    gPad->SetLogy();
+    gPad->SetTopMargin(0.02);
+    gPad->SetBottomMargin(0.16);
+    gPad->SetRightMargin(0.04);
+    gPad->SetLeftMargin(0.16);
+    
+    energy_iphinot39[ieta_bin][0]->SetMinimum(0.8);
+    energy_iphinot39[ieta_bin][0]->GetXaxis()->SetTitle("Energy (GeV)");
+    energy_iphinot39[ieta_bin][0]->GetYaxis()->SetTitle("Number of entries");
+    energy_iphinot39[ieta_bin][0]->SetTitleSize(0.08);
+    energy_iphinot39[ieta_bin][0]->SetTitleSize(0.08,"X");
+    energy_iphinot39[ieta_bin][0]->SetTitleSize(0.08,"Y");
+    energy_iphinot39[ieta_bin][0]->SetTitleOffset(1.0,"X");
+    energy_iphinot39[ieta_bin][0]->SetTitleOffset(1.0,"Y");
+    energy_iphinot39[ieta_bin][0]->SetLabelSize(0.06,"X");
+    energy_iphinot39[ieta_bin][0]->SetLabelSize(0.06,"Y");
+    energy_iphinot39[ieta_bin][0]->SetStats(false);
+    energy_iphinot39[ieta_bin][0]->SetLineColor(1);
+    
+    energy_iphinot39[ieta_bin][0]->SetLineColor(6);
+    energy_QIE8x2_iphi39[ieta_bin][0]->SetLineColor(2);
+    energy_QIE8calib_iphi39[ieta_bin][0]->SetLineColor(4);
+
+    sprintf(tmp,"ieta = %d, depth=1",ieta);
+    energy_iphinot39[ieta_bin][0]->SetTitle(tmp);
+    energy_iphinot39[ieta_bin][0]->Draw();
+    energy_QIE8x2_iphi39[ieta_bin][0]->Draw("same");
+    if (ieta!=29 && ieta!=41) energy_QIE8calib_iphi39[ieta_bin][0]->Draw("same");
+
+    if (ieta_bin==0){
+      catLeg1c->AddEntry(energy_iphinot39[ieta_bin][0],"iphi#neq39: Q #times gain","l");
+      catLeg1c->AddEntry(energy_QIE8x2_iphi39[ieta_bin][0],"iphi=39: Q_{B} #times gain #times 2");
+      //catLeg1c->AddEntry(energy_QIE8calib_iphi39[ieta_bin][0],"iphi=39: Q_{B} #times gain (calibrated)");
+    }
+    if (ieta_bin==1){
+      catLeg1c2->AddEntry(energy_iphinot39[ieta_bin][0],"iphi#neq39: Q #times gain","l");
+      catLeg1c2->AddEntry(energy_QIE8x2_iphi39[ieta_bin][0],"iphi=39: Q_{B} #times gain #times 2");
+      catLeg1c2->AddEntry(energy_QIE8calib_iphi39[ieta_bin][0],"iphi=39: Q_{B} #times gain (calibrated)");
+    }
+    if (ieta!=29 && ieta!=41) catLeg1c2->Draw();
+    else                      catLeg1c->Draw();
+
+  }
+  c1c->SaveAs("c_HFP_depth1_c.gif");
+  c1c->SaveAs("c_HFP_depth1_c.pdf");
+
+  //
+  // Plotting - Depth2
   //
   TCanvas *c2 = new TCanvas("c_HFP_depth2","c2_HFP_depth2",1600,1200);
   TLegend* catLeg2 = new TLegend(0.55,0.5,0.9,0.85);
@@ -138,23 +302,74 @@ void plot_spectra(){
     energy_iphi39[ieta_bin][1]->SetLineColor(1);
     energy_iphi35[ieta_bin][1]->SetLineColor(2);
     energy_iphi43[ieta_bin][1]->SetLineColor(4);
+    energy_iphinot39[ieta_bin][1]->SetLineColor(6);
 
     sprintf(tmp,"ieta = %d, depth=2",ieta);
     energy_iphi39[ieta_bin][1]->SetTitle(tmp);
     energy_iphi39[ieta_bin][1]->Draw();
     energy_iphi35[ieta_bin][1]->Draw("same");
     energy_iphi43[ieta_bin][1]->Draw("same");
+    //energy_iphinot39[ieta_bin][1]->Draw("same");
 
     if (ieta_bin==0){
       catLeg2->AddEntry(energy_iphi35[ieta_bin][1],"iphi=35: Q #times gain","l");
       catLeg2->AddEntry(energy_iphi39[ieta_bin][1],"iphi=39: Q_{A}+Q_{B} #times gain","l");
       catLeg2->AddEntry(energy_iphi43[ieta_bin][1],"iphi=43: Q #times gain","l");
+      //catLeg2->AddEntry(energy_iphi43[ieta_bin][1],"iphi#neq39: Q #times gain","l");
     }
     catLeg2->Draw();
+
+    std::cout << ieta << " "
+              << emean(energy_iphi35[ieta_bin][1],500.,50.) << " "
+              << emean(energy_iphi39[ieta_bin][1],500.,50.) << " "
+              << emean(energy_iphi43[ieta_bin][1],500.,50.) << std::endl;
+    relative_response_vsieta_iphi39_depth2->SetBinContent(ieta_bin+1,
+                                                   2.*emean(energy_iphi39[ieta_bin][1],emean_hi,emean_lo)
+                                                   /(emean(energy_iphi35[ieta_bin][1],emean_hi,emean_lo)+emean(energy_iphi43[ieta_bin][1],emean_hi,emean_lo)));
+    relative_response_vsieta_iphi39_depth2->SetBinError(ieta_bin+1,0.);
+    relative_response_iphi39_depth2->Fill(
+                                                   2.*emean(energy_iphi39[ieta_bin][1],emean_hi,emean_lo)
+                                                   /(emean(energy_iphi35[ieta_bin][1],emean_hi,emean_lo)+emean(energy_iphi43[ieta_bin][1],emean_hi,emean_lo)));
    
   }
   c2->SaveAs("c_HFP_depth2.gif");
   c2->SaveAs("c_HFP_depth2.pdf");
+
+  //
+  //
+  //
+  TCanvas *c2b = new TCanvas("c_HFP_depth2_b","c2b_HFP_depth2",1600,1200);
+  TLegend* catLeg2b = new TLegend(0.55,0.5,0.9,0.85);
+  catLeg2b->SetTextSize(0.064);
+  catLeg2b->SetTextFont(42);
+  catLeg2b->SetLineColor(0);
+
+  c2b->Divide(3,4);
+  for (int ieta_bin=0; ieta_bin<=12; ieta_bin++){
+    int ieta=ieta_bin+29;
+    if (ieta==40) continue;
+    if (ieta>40) c2b->cd(ieta_bin); // skip ieta=40
+    else         c2b->cd(ieta_bin+1);
+    gPad->SetLogy();
+    gPad->SetTopMargin(0.02);
+    gPad->SetBottomMargin(0.16);
+    gPad->SetRightMargin(0.04);
+    gPad->SetLeftMargin(0.16);
+
+    sprintf(tmp,"ieta = %d, depth=2",ieta);
+    energy_iphi39[ieta_bin][1]->SetTitle(tmp);
+    energy_iphi39[ieta_bin][1]->Draw();
+    energy_iphinot39[ieta_bin][1]->Draw("same");
+
+    if (ieta_bin==0){
+      catLeg2b->AddEntry(energy_iphi39[ieta_bin][1],"iphi=39: Q_{A}+Q_{B} #times gain","l");
+      catLeg2b->AddEntry(energy_iphinot39[ieta_bin][1],"iphi#neq39: Q #times gain","l");
+    }
+    catLeg2b->Draw();
+   
+  }
+  c2b->SaveAs("c_HFP_depth2_b.gif");
+  c2b->SaveAs("c_HFP_depth2_b.pdf");
 
   //
   // Plotting
@@ -257,6 +472,57 @@ void plot_spectra(){
   c4->Update();
   c4->SaveAs("c_HFP_iphi39_depth2_anodeAB.gif");
   c4->SaveAs("c_HFP_iphi39_depth2_anodeAB.pdf");
+
+  //
+  // Plotting
+  //
+  TCanvas *c5 = new TCanvas("c_relative_response_vsieta_iphi39","",800,500);
+  TLegend* catLeg5 = new TLegend(0.55,0.7,0.9,0.85);
+  catLeg5->SetTextSize(0.044);
+  catLeg5->SetTextFont(42);
+  catLeg5->SetLineColor(0);
+
+  //c5->Divide(1,2);
+    
+  //c5->cd(1);
+  gPad->SetTopMargin(0.02);
+  gPad->SetBottomMargin(0.16);
+  gPad->SetRightMargin(0.04);
+  gPad->SetLeftMargin(0.16);
+  
+  relative_response_vsieta_iphi39_depth1->SetMinimum(0.4);
+  relative_response_vsieta_iphi39_depth1->SetMaximum(1.5);
+
+  relative_response_vsieta_iphi39_depth1->GetXaxis()->SetTitle("ieta");
+  relative_response_vsieta_iphi39_depth1->GetYaxis()->SetTitle("<E>(iphi=39,Q_{A}+Q_{B}) / <E>(iphi=35,43)");
+  relative_response_vsieta_iphi39_depth1->SetTitle("");
+  relative_response_vsieta_iphi39_depth1->SetName("");
+  relative_response_vsieta_iphi39_depth1->SetTitleSize(0.001);
+  relative_response_vsieta_iphi39_depth1->SetTitleSize(0.05,"X");
+  relative_response_vsieta_iphi39_depth1->SetTitleSize(0.05,"Y");
+  relative_response_vsieta_iphi39_depth1->SetTitleOffset(1.0,"X");
+  relative_response_vsieta_iphi39_depth1->SetTitleOffset(1.0,"Y");
+  relative_response_vsieta_iphi39_depth1->SetLabelSize(0.04,"X");
+  relative_response_vsieta_iphi39_depth1->SetLabelSize(0.04,"Y");
+  relative_response_vsieta_iphi39_depth1->SetStats(false);
+  relative_response_vsieta_iphi39_depth1->SetLineColor(2);
+  relative_response_vsieta_iphi39_depth2->SetLineColor(4);
+
+  relative_response_vsieta_iphi39_depth1->SetMarkerStyle(20);
+  relative_response_vsieta_iphi39_depth2->SetMarkerStyle(23);
+  relative_response_vsieta_iphi39_depth1->Draw("E0");
+  relative_response_vsieta_iphi39_depth2->Draw("samee0");
+
+  catLeg5->AddEntry(relative_response_vsieta_iphi39_depth1,"HF+ long fiber (depth 1)","l");
+  catLeg5->AddEntry(relative_response_vsieta_iphi39_depth2,"HF+ short fiber (depth 2)","l");
+  catLeg5->Draw();
+
+  relative_response_iphi39_depth1->Print();
+  relative_response_iphi39_depth2->Print();
+  
+  c5->SaveAs("c_relative_response_vsieta_iphi39.gif");
+  c5->SaveAs("c_relative_response_vsieta_iphi39.pdf");
+
 
 }
 
